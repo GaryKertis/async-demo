4d9ff44557889607e80f4cc1459a6582c7bbe277
Gary Kertis <gary.kertis@gmail.com>
2021-11-15 17:38:56 -0500

    Clean it up.

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const getUserName = (id, callback) => {
   setTimeout(() => {
     if (id === 1) {
       callback("Gary");
-    }
-    if (id === 2) {
+    } else if (id === 2) {
       callback("Pete");
     }
   }, 1000);
@@ -14,15 +13,8 @@ const getFavoriteMovie = (user, callback) => {
   setTimeout(() => {
     if (user === "Gary") {
       callback("The Big Lebowski");
-    }
-    if (user === "Pete") {
+    } else if (user === "Pete") {
       callback("The Penguins of Madagascar");
     }
   }, 1000);
 };
-
-getUserName(1, (userName) => {
-  getFavoriteMovie(userName, (favoriteMovie) => {
-    console.log(favoriteMovie);
-  });
-});
